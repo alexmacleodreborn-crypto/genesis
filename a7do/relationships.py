9c413d31c0a82bb73fe2e1e6ad6cb61f6b3ef8cd
alexmacleodreborn-crypto <alexmacleodreborn@gmail.com>
2026-01-01 10:59:00 +0000

    Update relationships.py

--- a7do/relationships.py
+++ a7do/relationships.py
@@ -7,14 +7,12 @@ class Relationship:
     subject_id: str
     object_id: str
     rel_type: str
-    note: str
+    note: str = ""
 
 
 class RelationshipStore:
     def __init__(self):
         self.relations: List[Relationship] = []
 
-    def add(self, subject_id, object_id, rel_type, note=""):
-        self.relations.append(
-            Relationship(subject_id, object_id, rel_type, note)
-        )
+    def add(self, subject_id: str, object_id: str, rel_type: str, note: str = ""):
+        self.relations.append(Relationship(subject_id, object_id, rel_type, note))
