400e9de462f291b2213f7d93cd0e5f4a34c66904
alexmacleodreborn-crypto <alexmacleodreborn@gmail.com>
2026-01-01 18:22:01 +0000

    Update __init__.py

--- a7do/__init__.py
+++ a7do/__init__.py
@@ -1,3 +1,3 @@
-from .profiles import WorldProfiles
-from .schedule_engine import Schedule
-from .mind import A7DOMind
+from a7do.profiles import WorldProfiles
+from a7do.schedule_engine import Schedule
+from a7do.mind import A7DOMind
