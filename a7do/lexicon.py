c6aaa1ecafc18469fba3fbf458a08e2c3cd59ebd
alexmacleodreborn-crypto <alexmacleodreborn@gmail.com>
2026-01-01 17:22:37 +0000

    Update lexicon.py

--- a7do/lexicon.py
+++ a7do/lexicon.py
@@ -1,10 +1,26 @@
 class Lexicon:
+    """
+    Tracks word exposure by context.
+    """
+
     def __init__(self):
         self.words = {}
 
-    def learn(self, phrase):
-        for w in phrase.split():
-            self.words[w] = self.words.get(w, 0) + 1
+    def learn_from_event(self, event):
+        tokens = [
+            event.place,
+            event.agent,
+            event.action
+        ]
+
+        if event.object:
+            tokens.append(event.object)
+
+        tokens += event.emphasis
+
+        for t in tokens:
+            t = t.lower()
+            self.words[t] = self.words.get(t, 0) + 1
 
     def snapshot(self):
         return dict(self.words)
